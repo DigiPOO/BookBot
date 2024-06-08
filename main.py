a42abd1cf06c74a485590fd57d196c8827d261a3
DigiPOO <digitalprods@gmail.com>
2024-06-08 14:16:26 -0400

    updated main to open file, read file, and print the number of words

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world")
+def main():
+    #sets the location of the book
+    book_location = "books/frankenstein.txt"
+    #gets the text from the file in book_location
+    text = open_file(book_location)
+    #prints the number of words in text
+    print(get_num_words(text))
+
+def open_file (location):
+    #open the file
+    with open(location) as file:
+        #read the file
+        data = file.read()
+    #returns the read data    
+    return data
+
+def get_num_words(text):
+    #splits the text into words
+    words = text.split()
+    #returns the number of wrods in the text
+    return len(words)
+
+def count_letters(test):
+    return
+
+main()
