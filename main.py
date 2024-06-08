7d6797ffac29c21547fe747cc6c641b9b8c94fa1
DigiPOO <digitalprods@gmail.com>
2024-06-08 14:41:59 -0400

    added a cuntion to count the number of characters and store it into a dictionary

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     text = open_file(book_location)
     #prints the number of words in text
     print(get_num_words(text))
+    #get the count of all characters
+    chars_count = count_chars(text)
+    print(chars_count)
 
 def open_file (location):
     #open the file
@@ -20,7 +23,21 @@ def get_num_words(text):
     #returns the number of wrods in the text
     return len(words)
 
-def count_letters(test):
-    return
+def count_chars(text):
+    #creates a dictionary for storing the different occuring characters and their count
+    chars_count = {}
+    #for each of the characters in the text
+    for char in text:
+        #converts the character to lowercase if applicable
+        lowered = char.lower()
+        #if the character is aready in the dictionary, increase its count by 1
+        if lowered in chars_count:
+            chars_count[lowered] = chars_count[lowered] + 1
+        #else the character is not in the dictionary and sets it count to 1
+        else:
+            chars_count[lowered] = 1
+    #returns the dictionary
+    return chars_count
+
 
 main()
